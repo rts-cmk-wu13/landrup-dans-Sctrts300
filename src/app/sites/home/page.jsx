c58a38b3fd3d6c77ad9731c249f495f3a591a5c9
Sidsel Thomsen <U2469819@rts-365.dk>
2026-02-18 11:50:54 +0100

    still preparing

---
+++ src/app/sites/home/page.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div className="home-page">
+      
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/app/sites/login/page.jsx
@@ -0,0 +1,9 @@
+function login() {
+  return (
+    <section>
+
+    </section>
+    );
+}
+
+export default login;
