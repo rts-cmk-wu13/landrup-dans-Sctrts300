0bf7c384180b53c909b27301f3e4ab2f6d00ddf5
Sidsel Thomsen <U2469819@rts-365.dk>
2026-03-02 14:05:30 +0100

    teams works

--- src/app/components/admincards.jsx
+++ src/app/components/admincards.jsx
@@ -6,6 +6,7 @@ export default async function AdminPanel()
     const cookieStore = await cookies()
     const userId = cookieStore.get("userId")?.value
     const accessToken = cookieStore.get("accessToken")?.value
+    let activities = []
 
     const response = await fetch(`http://localhost:4000/api/v1/users/${userId}`, {
     headers: {
@@ -15,15 +16,45 @@ export default async function AdminPanel()
 
     const userData = await response.json()
 
-    return (
+console.log(userData)
+
+if (userData.role === 'instructor')  {
+
+    console.log(userData.id)
+
+    const activitiesResponse = await fetch(`http://localhost:4000/api/v1/activities`)
+    const activitiesData = await activitiesResponse.json()
+
+    console.log(activitiesData)
+    
+    activities = activitiesData.filter(activity => activity.instructorId === userData.id)
+}
+console.log(activities)
+
+
+    return userData.role === 'instructor' ? (
         <div>
             <h1>Mine hold</h1>
+            <ul>
+                {activities.map(activity => (
+                    <div key={activity.id}>
+                        <h2>{activity.name}</h2>
+                        <Link href={`/activities/${activity.id}`}>Deltagerliste</Link>
+                    </div>
+                ))}
+            </ul>
+        </div>
+
+    ) : (
+        <div>
+            <h1>Tilmeldte hold</h1>
             <ul>
                 {userData.activities.map(activity => (
                     <div key={activity.id}>
                         <h2>{activity.name}</h2>
                         <Link href={`/activities/${activity.id}`}>Vis hold</Link>
-                    </div>
+                        
+                    </div>  
                 ))}
             </ul>
         </div>
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -2,7 +2,7 @@ import { cookies } from "next/headers"
 import Link from "next/link"
 import Navbar from "../components/Navbar"
 import AdminPanel from "../components/admincards"
-import UserPanel from "../components/usercards"
+import "./profile.css"
 
 
 
@@ -27,8 +27,8 @@ const response = await fetch(`http://localhost:4000/api/v1/users/${userId}`, {
             <p>{userData.firstname} {userData.lastname}</p>
             <p>{userData.role}</p>
 
-            {userData.role === 'instructor' && <AdminPanel />}
-            {userData.role === 'default' && <UserPanel />}
+            <AdminPanel />
+            
             <Navbar />
         </div>
 
@@ -36,17 +36,3 @@ const response = await fetch(`http://localhost:4000/api/v1/users/${userId}`, {
 
     )
 }
-
-        // <div>
-        //     <h1>Profile</h1>
-        //     <p>Velkommen</p>
-
-        //         <p>Du har ikke adgang til denne side. Log ind for at se din profil.</p>
-
-        //     <p>{userData.firstname} {userData.lastname}</p>
-        //     {userData.activities.map(activity => (
-        //         <div key={activity.id}>
-        //             <h2>{activity.name}</h2>
-        //             <Link href={`/activities/${activity.id}`}>Hvis hold</Link>
-        //         </div>
-// , {userId}
